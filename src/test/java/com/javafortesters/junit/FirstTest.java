e341caabd07891394b927855a440656f866b28bd
Ernesto Albarez <ernestoalbrz@gmail.com>
2018-08-16 15:03:44 -0300

    First commit to improve my automation skills

---
+++ src/test/java/com/javafortesters/junit/FirstTest.java
@@ -0,0 +1,31 @@
+package com.javafortesters.junit;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import static junit.framework.Assert.assertTrue;
+
+public class FirstTest {
+
+    @Test
+    public void driverIsTheKing(){
+        WebDriver driver = new  HtmlUnitDriver(); //Headless browser
+
+        driver.get("http://www.compendiumdev.co.uk/selenium");
+
+        assertTrue(driver.getTitle().startsWith("Selenium Simplified")); //Test Pass
+        //assertTrue(driver.getTitle().startsWith("Selenium Ximplified")); //Test Fails
+    }
+
+    @Test
+    public void firefoxIsSuportedByWebDriver(){
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("http://www.compendiumdev.co.uk/selenium");
+
+        assertTrue(driver.getTitle().startsWith("Selenium Simplified"));
+
+        driver.close();
+    }
+}
