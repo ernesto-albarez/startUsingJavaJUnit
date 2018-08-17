40b531cd60ea91396847b2d7c1755865ee55b710
Ernesto Albarez <ernestoalbrz@gmail.com>
2018-08-17 17:02:23 -0300

    Added geckodriver for Firefox

--- src/test/java/com/javafortesters/junit/FirstTest.java
+++ src/test/java/com/javafortesters/junit/FirstTest.java
@@ -3,7 +3,6 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-
 import static junit.framework.Assert.assertTrue;
 
 public class FirstTest {
@@ -11,21 +10,22 @@ public class FirstTest {
     @Test
     public void driverIsTheKing(){
         WebDriver driver = new  HtmlUnitDriver(); //Headless browser
-
         driver.get("http://www.compendiumdev.co.uk/selenium");
 
         assertTrue(driver.getTitle().startsWith("Selenium Simplified")); //Test Pass
-        //assertTrue(driver.getTitle().startsWith("Selenium Ximplified")); //Test Fails
+        //assertTrue(driver.getTitle().startsWith("Random Text")); //Test Fails
     }
 
     @Test
     public void firefoxIsSupportedByWebDriver(){
-        WebDriver driver = new FirefoxDriver();
+        //if you didn't update the Path system variable to add the full directory path to the executable
+        //then doing this directly through code:
+        System.setProperty("webdriver.firefox.marionette", "drivers/geckodriver.exe");
 
+        WebDriver driver = new FirefoxDriver();
         driver.get("http://www.compendiumdev.co.uk/selenium");
 
         assertTrue(driver.getTitle().startsWith("Selenium Simplified"));
-
         driver.close();
     }
 }
